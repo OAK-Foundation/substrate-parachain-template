b94b759a412e2be9253d480daf468824faa94b62
Ryan Huttman <ryan@oak.tech>
2022-09-22 11:16:54 -0700

    Removing unused tests and benchmarks (#18)

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -14,15 +14,6 @@ use xcm::latest::prelude::*;
 use oak_xcm::XcmInstructionGenerator;
 pub use pallet::*;
 
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
-
 #[frame_support::pallet]
 pub mod pallet {
 
